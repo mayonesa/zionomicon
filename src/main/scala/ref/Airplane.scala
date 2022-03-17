d7e63a1e3d325ea39db8eadff393e6d5edb0e409
juancho <juanchoj1024@gmail.com>
2022-03-17 13:01:38 -0400

    airplane sol'n

--- src/main/scala/ref/Airplane.scala
+++ src/main/scala/ref/Airplane.scala
@@ -1,11 +1,18 @@
 package ref
 
-import zio.ZIOAppDefault
+import zio.{ZIOAppDefault, Ref, Console}
 
 object Airplane extends ZIOAppDefault:
-  private val seats = ???
 
-  def run = ???
+  private val seats = Ref.make(Vector.fill(10)(Option.empty[String]))
+  // yep
+  def run =
+    for
+      seating <- seats
+      _ <- seating.update(_.updated(1, Some("juancho")))
+      vector <- seating.get
+      _ <- Console.printLine(vector)
+    yield ()
 
   def book(i: Int) = ???
   
